8b3fa9eecb3f36a958e7f77f236f144d56829dc1
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-05-23 10:29:49 +0300

    creates hero

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,66 @@
 <template>
-<div class="home">
-    Home
-</div>
+  <div class="home">
+
+    <div class="section">
+        <div class="columns">
+            <div class="column is-6 pr-0">
+                <p class="title is-large pd-0">Hy! I Am</p>
+                <p class="title is-large">Kelvin Njoro</p>
+                <p class="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque id omnis voluptas,
+                    fuga culpa itaque natus dolor doloremque, dolore eligendi, vero corporis ex. Pariatur fugiat minima
+                    nisi ea nobis dolorem!</p>
+                <div class="paybtn">
+                    <div class="buttons is-large">
+                        <button>Hire me</button>
+                    </div>
+                    <ul class="payments">
+                        <li class="paypal">
+                            <img src="@/assets/payment/Paypal.jpg" alt="">
+                        </li>
+                        <li class="mastCard">
+                            <img src="@/assets/payment/MasterCard.jpg" alt="">
+                        </li>
+                        <li class="skrill">
+                            <img src="@/assets/payment/Skrill.png" alt="">
+                        </li>
+                        <li class="visa">
+                            <img src="@/assets/payment/Visa.jpg" alt="">
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+            <div class="column is-4">
+                <div class="profile">
+                    <div class="image is-large ">
+                        <img src="@/assets/profilePic/dp.png" class="profileImg" alt="home">
+                    </div>
+                    <div class="bigCircle">
+                        <div class="google">
+                            <img src="@/assets/socialIcons/github.png" alt="google">
+                        </div>
+                    </div>
+                    <div class="smallCircle">
+                        <div class="twitter">
+                            <img src="@/assets/socialIcons/linkedin.png" alt="linkendin">
+                        </div>
+                    </div>
+                    <div class="facebook">
+                        <img src="@/assets/socialIcons/twitter.png" alt="twitter">
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+  </div>
 </template>
+
+<script>
+</script>
+
+<style scoped>
+
+</style>
