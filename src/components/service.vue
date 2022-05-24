529466af10f60587c8d1ba941c7474533af90424
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-05-24 23:38:11 +0300

    creates the structure to show jobs and projects done adds style of the rectangles appearance uses color differences on divs

--- src/components/service.vue
+++ src/components/service.vue
@@ -2,13 +2,40 @@
   <div class="service">
     <div class="section">
       <div class="columns cards">
-        <div class="column is-4 pr-0">
+        <div class="column is-4 card">
+          <div class="projects">
+            <div class="success">
+            <div class="firstRectangle">
+              <div class="secondRectangle">
+
+            </div>
+            <div class="circle">
+
+            </div>
+              
+            </div>
+            
+
+            </div>
+            <div class="success">
+            <div class="firstRectangle">
+              <div class="secondRectangle">
+
+            </div>
+            <div class="circle">
+
+            </div>
+              
+            </div>
+            
+
+            </div>
+            </div>
+        </div>
+        <div class="column is-4 card">
             service
         </div>
-        <div class="column is-4 pr-0">
-            service
-        </div>
-        <div class="column is-4 pr-0">
+        <div class="column is-4 card">
             service
         </div>
       </div>
@@ -22,4 +49,52 @@ export default {
 }
 </script>
 
-<style></style>
+<style scoped>
+.cards .card{
+  border: 1px solid black;
+  height: 20em;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.projects{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+
+}
+.projects .success{
+  width: 100%;
+}
+.projects .success .firstRectangle{
+  height: 5em;
+  background-color: red;
+  border-radius: .6em;
+  position: relative;
+}
+.projects .success .firstRectangle .secondRectangle{
+  position: absolute;
+   width: 80%;
+  height: 100%;
+  background-color: rgb(0, 0, 0);
+  border-radius: .6em;
+  left: 50%;
+  transform: translate(-50%, 0);
+  top: -20%;
+
+}
+.projects .success .firstRectangle .circle{
+  position: absolute;
+   width: 4em;
+  height: 4em;
+  background-color: rgb(129, 24, 24);
+  border-radius: 50%;
+  left: 50%;
+  top: -50%;
+  transform: translate(-50%, -30%);
+
+}
+</style>
