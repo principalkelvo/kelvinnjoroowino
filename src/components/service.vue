33427c6c097fd75b83a9e36fa264269db5b4ac06
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-05-24 22:30:02 +0300

    adds sercice component

---
+++ src/components/service.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="service">
+    <div class="section">
+      <div class="columns cards">
+        <div class="column is-4 pr-0">
+            service
+        </div>
+        <div class="column is-4 pr-0">
+            service
+        </div>
+        <div class="column is-4 pr-0">
+            service
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    name: "Service",
+
+}
+</script>
+
+<style></style>
