0ae78cfe83734e858d4bfde618408b347165adc4
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-05-26 18:05:31 +0300

    creates a card

--- src/components/services.vue
+++ src/components/services.vue
@@ -14,7 +14,7 @@
             <div class="column is-6">
                 <div class="card">
                     <div class="cardTitle">
-                        <img src="#" alt="">
+                        <img src="../assets/emoji/love.png" alt="">
                         <h2>Design</h2>
                     </div>
                     <div class="cardContent">
@@ -22,7 +22,7 @@
                     </div>
                     <div class="cardFooter">
                         <div class="buttons">
-                            <button>Learn More</button>
+                            <button class="no-fill">Learn More</button>
                         </div>
                     </div>
                 </div>
@@ -39,6 +39,8 @@
 </script>
 
 <style scoped>
+
+
 .column .title h1{
     font-size: 3em;
     margin: 0;
@@ -48,5 +50,47 @@
     margin-top: 0;
     color: #faaa53;
 }
+
+.card{
+    background-color: #fff;
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px rgba(0,0,0,0.1);
+    padding: 1rem;
+    margin-bottom: 2rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 15em;
+    height: 16em;
+}
+
+.cardTitle{
+
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+}
+.card .cardTitle h2{
+    font-size: 1.5em;
+    margin: 0;
+}
+
+.cardContent{
+    padding: 1rem;
+}
+
+.cardContent p{
+    text-align: center;
+
+}
+
+.buttons button.no-fill{
+    text-transform: uppercase;
+    font-weight: 400;
+    font-size: .8em;
+    /* box-shadow: 2px 4px 73px 91px #A989E540; */
+
+}
+
 
 </style>
