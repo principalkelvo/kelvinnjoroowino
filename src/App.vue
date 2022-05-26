1901e82dcf335e5db8b0b52b20948fe0d0948e25
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-05-26 18:04:31 +0300

    adds no fill button background color

--- src/App.vue
+++ src/App.vue
@@ -69,16 +69,22 @@ export default{
 
 /* button no-fill */
 .buttons button.no-fill {
-    color: #000;
+    color: #a989e5;
     background: #fff;
-    border: 1px solid #000;
+    border: .5px solid #00000012;
+    box-shadow: 0px 0px 0px 0px inset #FAAA53,
+                2px 4px 3.041em 4em #A989E540;
+
+    transition: ease-out 0.3s;
 }
 
 .buttons button.no-fill:hover,
 .buttons button.no-fill:focus,
 .buttons button.no-fill:active
  {
-    box-shadow: inset 200px 0 0 0 #FAAA53;
+    box-shadow: inset 10em 0 0 0 #FAAA53,
+                0px 19px 28px -11px #00000040,
+                2px 4px 73px 91px #A989E540;
     color: #fff !important;
     border: 1.45px solid #FAAA53;
 }
