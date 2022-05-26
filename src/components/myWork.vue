bb93fad48b538d4d5369c7a7aaac75b28e8efc34
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-05-26 20:59:45 +0300

    adds cards adds overflow to cards imports buttons

--- src/components/myWork.vue
+++ src/components/myWork.vue
@@ -19,6 +19,7 @@
                                 <img src="../assets/emoji/next.svg" alt="">
                             </div>
                         </div>
+                        <div class="allCards">
                         <div class="card">
 
                         </div>
@@ -28,6 +29,12 @@
                         <div class="card">
                             
                         </div>
+                        
+                        <div class="card">
+                            
+                        </div>
+                        </div>
+                        
                     </div>
                 </div>
         </div>
@@ -48,4 +55,71 @@ export default {
 </script>
 
 <style scoped>
+/* title  */
+.columns .title{
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: left;
+    justify-content: left;
+    padding: 3rem;
+}
+.columns .title h1 span{
+    color: #faaa53;
+}
+.columns .title h1 {
+    margin: 0;
+}
+.columns .cards{
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 3rem;
+}
+
+.columns .cards .buttons{
+    width: 10em;
+    height: 16em;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 1rem;
+
+    
+}
+
+.columns .cards .buttons .prevBtn,
+.columns .cards .buttons .nextBtn{
+    min-width: 8em;
+    height: 6em;
+    display: flex;
+    justify-content: center;
+}
+
+/* .columns .cards .buttons .prevBtn,
+.columns .cards .buttons .nextBtn */
+
+.columns .cards .allCards{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    overflow-x: scroll;
+}
+
+.columns .cards .card{
+    background-color: #fff;
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px rgba(0,0,0,0.1);
+    padding: 1rem;
+    margin: 1rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    min-width: 12em;
+    width: 15em;
+    height: 16em;
+    min-height: 13em;
+}
 </style>
