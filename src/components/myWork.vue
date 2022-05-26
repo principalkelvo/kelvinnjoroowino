8d312d467293331128e0aa3afaff79ac90599c56
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-05-26 19:48:58 +0300

    creates a structure for my work adds classes to divs

---
+++ src/components/myWork.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="myWork">
+        <div class="section">
+            <div class="columns">
+                <div class="title">
+                    <h1 >Clients get <span>always exceptional</span></h1>
+                    <h1>works from me....</h1>
+                </div>
+                
+            </div>
+            <div class="columns">
+                    <div class="cards">
+                        <div class="buttons">
+                            <div class="prevBtn">
+                                <img src="../assets/emoji/prev.svg" alt="">
+
+                            </div>
+                            <div class="nextBtn">
+                                <img src="../assets/emoji/next.svg" alt="">
+                            </div>
+                        </div>
+                        <div class="card">
+
+                        </div>
+                        <div class="card">
+                            
+                        </div>
+                        <div class="card">
+                            
+                        </div>
+                    </div>
+                </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'myWork',
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style scoped>
+</style>
