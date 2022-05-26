54fcf75c2d287fe32cebd8bc93e93f4f27a091c1
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-05-26 17:33:35 +0300

    services section component

---
+++ src/components/services.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="services">
+    <div class="section">
+        <div class="columns">
+            <div class="column is-6">
+                <div class="title">
+                    <h1>My awesome</h1>
+                    <h2>Services</h2>
+                </div>
+                <div class="buttons">
+                    <button>Download CV</button>
+                </div>
+            </div>
+            <div class="column is-6">
+                <div class="card">
+                    <div class="cardTitle">
+                        <img src="#" alt="">
+                        <h2>Design</h2>
+                    </div>
+                    <div class="cardContent">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, atque?</p>
+                    </div>
+                    <div class="cardFooter">
+                        <div class="buttons">
+                            <button>Learn More</button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+</script>
+
+<style scoped>
+.column .title h1{
+    font-size: 3em;
+    margin: 0;
+}
+.column .title h2{
+    font-size: 2em;
+    margin-top: 0;
+    color: #faaa53;
+}
+
+</style>
