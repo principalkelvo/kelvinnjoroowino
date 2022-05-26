6f4cf32e7ad77d7e123a85bd053b652c8a05e494
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-05-26 22:32:15 +0300

    adds content and comments to the main skill structure

--- src/components/skills.vue
+++ src/components/skills.vue
@@ -3,13 +3,16 @@
         <div class="section">
             <div class="columns">
                 <div class="column is-6">
+                    <!-- title  -->
                     <div class="title">
                         <h1>Skills Used For All This</h1>
                         <h1>Brand & Client</h1>
                     </div>
+                    <!-- content  -->
                     <div class="content">
                         <p>Sit amet consectetur, adipisicing elit. Sit inventore magnam explicabo pariatur! Ipsa, soluta possimus. Saepe eum voluptatem deserunt neque nam nesciunt quasi, dolore dolorem quidem. Molestias veniam facere incidunt illum animi dolorum deserunt provident assumenda quidem?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, reprehenderit.</p>
                     </div>
+                    <!-- hire button  -->
                     <div class="buttons">
                         <button>Hire Me</button>
                     </div>
@@ -17,12 +20,19 @@
                 </div>
                 <div class="column is-6">
                     <div class="mainCircle">
-                        <div class="socialCircle">
 
+                    <!-- used technologies -->
+                        <div class="socialCircle">
+                            <div class="circle"></div>
+                            <div class="circle"></div>
+                            <div class="circle"></div>
+                            <div class="circle"></div>
                         </div>
+                        <!-- background purple circle -->
                         <div class="purpleCircle">
 
                         </div>
+                        <!-- background orange circle -->
                         <div class="orangeCircle">
 
                         </div>
@@ -33,3 +43,9 @@
         
     </div>
 </template>
+<script>
+
+</script>
+<style scoped>
+
+</style>
