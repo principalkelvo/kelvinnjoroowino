f8e5db7fe5226b5825e739c4074e40470d519f84
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-05-26 21:26:48 +0300

    structures skills and social icons

---
+++ src/components/skills.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="skills">
+        <div class="section">
+            <div class="columns">
+                <div class="column is-6">
+                    <div class="title">
+                        <h1>Skills Used For All This</h1>
+                        <h1>Brand & Client</h1>
+                    </div>
+                    <div class="content">
+                        <p>Sit amet consectetur, adipisicing elit. Sit inventore magnam explicabo pariatur! Ipsa, soluta possimus. Saepe eum voluptatem deserunt neque nam nesciunt quasi, dolore dolorem quidem. Molestias veniam facere incidunt illum animi dolorum deserunt provident assumenda quidem?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, reprehenderit.</p>
+                    </div>
+                    <div class="buttons">
+                        <button>Hire Me</button>
+                    </div>
+                    
+                </div>
+                <div class="column is-6">
+                    <div class="mainCircle">
+                        <div class="socialCircle">
+
+                        </div>
+                        <div class="purpleCircle">
+
+                        </div>
+                        <div class="orangeCircle">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</template>
