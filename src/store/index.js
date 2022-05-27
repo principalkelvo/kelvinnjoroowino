c453df6d3948cf0619b7a50e6728e002e5495c1c
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-05-27 18:13:47 +0300

    changes store file to index file adds darkmode mutation

---
+++ src/store/index.js
@@ -0,0 +1,27 @@
+import { createStore } from "vuex";
+export default createStore({
+    state: {
+        // Dark Mode
+        isDarkModeActive:false
+    },
+    mutations: {
+        // Dark Mode toggle
+        darkModeToggle(state, payload = null){
+            const htmlClassName = 'is-dark-mode-active';
+
+            state.isDarkModeActive = !state.isDarkModeActive;
+            if(state.isDarkModeActive){
+                document.documentElement.classList.add(htmlClassName);
+            }
+            else{
+                
+                document.documentElement.classList.remove(htmlClassName);
+            }
+        }
+        
+    },
+    actions: {
+    }, 
+    getters: {
+    }
+})
