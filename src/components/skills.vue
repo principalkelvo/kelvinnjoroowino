8a0233d20c990719d055715ee7fdb06a41f9f1a8
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-05-27 14:47:57 +0300

    adds id  to link section with navbar links

--- src/components/skills.vue
+++ src/components/skills.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="skills">
+    <div class="skills" id="skills">
         <div class="section">
             <div class="columns">
                 <div class="column is-6">
