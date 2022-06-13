4c6634197cb35a6570ab808ece9506f169c8c108
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-06-14 00:41:33 +0300

    adds first styling

--- src/components/customersFeedback.vue
+++ src/components/customersFeedback.vue
@@ -6,12 +6,12 @@
                     <div class="card">
                         <div class="cardHeader">
                             <div class="profileImg">
-
+                                <img src="#" alt="">
                             </div>
                         </div>
                         <div class="cardBody">
                             <div class="feedback">
-
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti accusamus cum non fugiat quod iure minima distinctio molestiae, voluptas assumenda.</p>
                             </div>
                         </div>
                     </div>
@@ -28,5 +28,39 @@ export default {
 
 </script>
 
-<style>
+<style scoped>
+.column.is-12{
+    width: 100%;
+}
+.card{
+    width: 70%;
+    margin: 2rem;
+    border: 2px solid black;
+    padding: 1em;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+}
+.card .cardHeader{
+    width: 70%;
+}
+.card .cardHeader .profileImg{
+    width: 5em;
+    height: 5em;
+    border-radius: 50%;
+}
+.card .cardHeader .profileImg img{
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+.card .cardBody{
+    width:90%;
+}
+.card .cardBody .feedback{
+    width: 100%;
+    margin: .3em;
+    border: 2px solid red;
+}
 </style>
