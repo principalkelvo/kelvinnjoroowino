5c9775ed561c5ac991b9823b4ce1334a6f3b6715
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-06-14 01:32:04 +0300

    adds style for the form

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -3,14 +3,16 @@
         <div class="section">
             <div class="columns">
                 <div class="column is-6">
+                    <div class="title">
                     <h1>Get In Touch</h1>
                     <h2>Contact Me</h2>
+                    </div>
                 </div>
                 <div class="column is-6">
                     <form action="" method="post">
                         <input type="name" placeholder="Name">
-                        <input type="name" placeholder="Name">
-                        <textarea name="message" id="message" cols="30" rows="10" placeholder="Message"></textarea>
+                        <input type="email" placeholder="E-mail">
+                        <textarea name="message" id="message" cols="21" rows="4" placeholder="Message"></textarea>
                         <div class="buttons">
                             <button>Send</button>
                         </div>
@@ -30,4 +32,50 @@ export default{
 </script>
 
 <style>
+.section .columns{
+    align-items: flex-start;
+}
+
+.column .title h1 {
+    font-size: 3em;
+    margin: 0;
+}
+.column .title h2 {
+    font-size: 2em;
+    margin-top: 0;
+    color: #faaa53;
+}
+.is-dark-mode-active .column .title h2{
+    color: #364cf3;
+}
+form input{
+    margin: 1em 0;
+    padding: .5em;
+    border: 2px solid #faaa53;
+    border-radius: .412em;
+}
+.is-dark-mode-active form input{
+    border: 2px solid #364cf3;
+}
+
+form textarea{
+    padding: .5em;
+    border: 2px solid #faaa53;
+    border-radius: .412em;
+}
+.is-dark-mode-active form textarea{
+    border: 2px solid #364cf3;
+}
+
+form{
+    display: flex;
+    flex-direction: column;
+    width: 80%;
+}
+
+@media only screen and (max-width: 769px) { 
+    form{
+    width: 100%;
+}
+}
 </style>
