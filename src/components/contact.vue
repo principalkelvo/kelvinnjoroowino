38512b9d048ca179fe3f6fe0a6bec80c19684b0c
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-06-14 00:15:35 +0300

    creates a contact section component

---
+++ src/components/contact.vue
@@ -0,0 +1,20 @@
+<template>
+ <div class="contact" id="contact">
+        <div class="section">
+            <div class="columns">
+                <div class="column is-6">
+                </div>
+            </div>
+        </div>
+ </div>
+</template>
+
+<script>
+export default{
+    name: "Contact",
+}
+
+</script>
+
+<style>
+</style>
