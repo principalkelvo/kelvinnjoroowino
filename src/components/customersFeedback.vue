8d4c8d43a1b44de309a6d8cc56fd160c19d50207
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-06-14 00:15:14 +0300

    creates a template skeleton for the customers feedback

---
+++ src/components/customersFeedback.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="customer" id="customer">
+        <div class="section">
+            <div class="columns">
+                <div class="column is-12">
+                    <div class="card">
+                        <div class="cardHeader">
+                            <div class="profileImg">
+
+                            </div>
+                        </div>
+                        <div class="cardBody">
+                            <div class="feedback">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CustomersFeedback",
+}
+
+</script>
+
+<style>
+</style>
