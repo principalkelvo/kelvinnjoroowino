46089befaf39e3d42f6e32a93d1ea525f2d27aac
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-06-14 01:05:22 +0300

    creates a form submit

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -3,6 +3,19 @@
         <div class="section">
             <div class="columns">
                 <div class="column is-6">
+                    <h1>Get In Touch</h1>
+                    <h2>Contact Me</h2>
+                </div>
+                <div class="column is-6">
+                    <form action="" method="post">
+                        <input type="name" placeholder="Name">
+                        <input type="name" placeholder="Name">
+                        <textarea name="message" id="message" cols="30" rows="10" placeholder="Message"></textarea>
+                        <div class="buttons">
+                            <button>Send</button>
+                        </div>
+                    </form>
+
                 </div>
             </div>
         </div>
