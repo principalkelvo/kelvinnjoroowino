66f4e366c29ebddc1eca8a7ad9a6a888741d1d26
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-06-27 23:26:52 +0300

    import Vuetestimonial globally

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import App from './App.vue'
 import router from '@/router'
 
 
+import VueTestimonials from "vue-testimonials"
+import 'vue-testimonials/dist/style.css'
+
 import store from '@/store'
 
 createApp(App)
 .use(store)
 .use(router)
+.use(VueTestimonials)
 .mount('#app')
