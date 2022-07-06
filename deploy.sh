7f8eaa7332c476934d9626478003394de75c60cb
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-07-07 00:33:35 +0300

    changed to master

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:principalkelvo/kelvinnjoroowino.git main:gh-pages
+ git push -f git@github.com:principalkelvo/kelvinnjoroowino.git master:gh-pages
 # https://principalkelvo.github.io/kelvinnjoroowino/
 cd -
