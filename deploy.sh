387cfd3cd67255253ed2034963a1362d78789a85
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-07-07 00:38:29 +0300

    deploy to https

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:principalkelvo/kelvinnjoroowino.git master:gh-pages
+ git push -f https://github.com/principalkelvo/kelvinnjoroowino.git master:gh-pages
+#  https://github.com/principalkelvo/kelvinnjoroowino.git
 # https://principalkelvo.github.io/kelvinnjoroowino/
 cd -
