0335a1eac8f8af84642a470c6e67037f3009d428
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-07-09 01:01:08 +0300

    change px to rem

--- src/App.vue
+++ src/App.vue
@@ -71,7 +71,7 @@ export default {
 
   <Navbar />
   <!-- <div class="container"> -->
-    <router-view />
+  <router-view />
   <!-- </div> -->
   <NavbarMobile />
 </template>
@@ -116,8 +116,8 @@ export default {
   outline: none;
   border: none;
   cursor: pointer;
-  box-shadow: 0px 0px 0px 0px inset #FAAA53,
-    0px 19px 28px -11px #00000040;
+  box-shadow: 0 0 0 0 inset #FAAA53,
+    0 1.187rem 1.75rem -0.6875rem #00000040;
 
   padding: .5rem 1rem;
   border-radius: .212rem;
@@ -132,8 +132,9 @@ export default {
 
 .is-dark-mode-active .buttons button {
   background: linear-gradient(180deg, #394aca 0%, #1429c9e1 100%);
-  box-shadow: 0px 0px 0px 0px inset #333e94,
-    0px 19px 28px -11px #00000040;
+  box-shadow: 0 0 0 0 inset #333e94,
+    0 1.1875rem 1.75rem -0.6875rem #00000040;
+
   color: #c4c4c4;
 
 }
@@ -141,32 +142,32 @@ export default {
 .buttons button:hover,
 .buttons button:focus,
 .buttons button:active {
-  box-shadow: inset 9em 0px 0 0 #fff;
-  border: 1px solid #000;
+  box-shadow: inset 9rem 0 0 0 #fff;
+  border: .0625rem solid #000;
   color: #000 !important;
 }
 
 .is-dark-mode-active .buttons button:hover,
 .is-dark-mode-active .buttons button:focus,
 .is-dark-mode-active .buttons button:active {
-  box-shadow: inset 9em 0px 0 0 #c4c4c4;
-  border: 1px solid #333e94;
+  box-shadow: inset 9rem 0 0 0 #c4c4c4;
+  border: 0.0625rem solid #333e94;
   color: #000000 !important;
 }
 
 .buttons.small button:hover,
 .buttons.small button:focus,
 .buttons.small button:active {
-  box-shadow: inset 6.1rem 0px 0 0 #fff;
-  border: 1px solid #000;
+  box-shadow: inset 6.1rem 0 0 0 #fff;
+  border: 0.0625rem solid #000;
   color: #000 !important;
 }
 
 .is-dark-mode-active .buttons.small button:hover,
 .is-dark-mode-active .buttons.small button:focus,
 .is-dark-mode-active .buttons.small button:active {
-  box-shadow: inset 6.1rem 0px 0 0 #c4c4c4;
-  border: 1px solid #333e94;
+  box-shadow: inset 6.1rem 0 0 0 #c4c4c4;
+  border: 0.0625rem solid #333e94;
   color: #000000 !important;
 }
 
@@ -174,9 +175,9 @@ export default {
 .buttons button.no-fill {
   color: #a989e5;
   background: #fff;
-  border: .5px solid #00000012;
-  box-shadow: 0px 0px 0px 0px inset #FAAA53,
-    2px 4px 3.041em 4em #A989E540;
+  border: .0313rem solid #00000012;
+  box-shadow: 0 0 0 0 inset #FAAA53,
+    0.125rem 0.25rem 3.041em 4em #A989E540;
 
   margin: 1rem;
 
@@ -186,29 +187,29 @@ export default {
 .is-dark-mode-active .buttons button.no-fill {
   color: #000;
   background: transparent;
-  border: .5px solid #00000012;
-  box-shadow: 0px 0px 0px 0px inset #333e94,
-    2px 4px 6.041em 2em #160d2540;
+  border: .0313rem solid #00000012;
+  box-shadow: 0 0 0 0 inset #333e94,
+    0.125rem 0.25rem 6.041em 2em #160d2540;
 }
 
 .buttons button.no-fill:hover,
 .buttons button.no-fill:focus,
 .buttons button.no-fill:active {
   box-shadow: inset 10.5em 0 0 0 #FAAA53,
-    0px 19px 28px -11px #00000040,
-    2px 4px 73px 91px #FAAA5340;
+    0 1.1875rem 1.75rem -0.6875rem #00000040,
+    0.125rem 0.25rem 4.5625rem 5.6875rem #FAAA5340;
   color: #fff !important;
-  border: 1.45px solid #FAAA53;
+  border: 0.0906rem solid #FAAA53;
 }
 
 .is-dark-mode-active .buttons button.no-fill:hover,
 .is-dark-mode-active .buttons button.no-fill:focus,
 .is-dark-mode-active .buttons button.no-fill:active {
   box-shadow: inset 10em 0 0 0 #333e94,
-    0px 19px 28px -11px #00000040,
-    2px 4px 73px 91px #333e9440;
+    0 1.1875rem 1.75rem -0.6875rem #00000040,
+    0.125rem 0.25rem 4.5625rem 5.6875rem #333e9440;
   color: #c4c4c4 !important;
-  border: 1.45px solid #333e94;
+  border: .0906rem solid #333e94;
 }
 
 /* Responsiveness */
