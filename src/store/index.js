7b88e6c6e9dccd13de0e6a4b223fc3c151fd0107
principal kelvo <principalkelvo@gmail.com>
2022-11-28 03:13:26 +0300

    checks localstorage for dark mode and initialises the state listens for darkmode toggles

--- src/store/index.js
+++ src/store/index.js
@@ -4,15 +4,33 @@ import {
 export default createStore({
     state: {
         // Dark Mode
-        isDarkModeActive: false
+        isDarkModeActive: " "
     },
     mutations: {
         // Dark Mode toggle
         /* Dark Mode */
+        initialiseStore(state) {
+            if (localStorage.getItem('Dark')) {
+                const htmlClassName = 'is-dark-mode-active'
+                // Replace the state object with the stored item
+                state.isDarkModeActive = JSON.parse(localStorage.getItem("Dark"))
+                if (state.isDarkModeActive) {
+                    document.documentElement.classList.add(htmlClassName)
+                } else {
+                    document.documentElement.classList.remove(htmlClassName)
+                }
+            }
+            else {
+                state.isDarkModeActive = false
+            }
+        },
+
         darkModeToggle(state, payload = null) {
             const htmlClassName = 'is-dark-mode-active'
 
             state.isDarkModeActive = !state.isDarkModeActive
+
+            localStorage.setItem("Dark", state.isDarkModeActive)
 
             if (state.isDarkModeActive) {
                 document.documentElement.classList.add(htmlClassName)
