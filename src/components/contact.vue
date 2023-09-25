542c6c9e452fa233da701228041d625e5944bbd7
principalkelvo <kelvinnjoroowino@gmail.com>
2023-09-25 23:22:33 +0300

    balance colors

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -58,19 +58,18 @@ export default {
             const isValid = await this.validateEmail(this.form.email)
             // const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
             // const isValid = false
-            // console.log("send email", isValid)
+            
 
             if (isValid) {
                 this.form.error = '';
                 this.validEmail = false
-                // console.log("sending email", isValid)
                 emailjs.sendForm('service_mvn3n88', 'template_yttdlvj', this.$refs.form, 'Qj_lR13PPkYyF676p')
                     .then((result) => {
                         console.log('SUCCESS!', result.text);
                         this.form.name = ""
                         this.form.email = ""
                         this.form.message = ""
-                        // console.log("sent email", isValid)
+                        
                         alert("Thank you")
                     }, (error) => {
                         console.log('FAILED...', error.text);
@@ -79,7 +78,6 @@ export default {
             else {
                 this.form.error = 'Invalid Email';
                 this.validEmail = true
-                // console.log(this.form.error, this.validEmail)
             }
 
             this.isActive = false
@@ -95,22 +93,13 @@ export default {
             const apiResponse = await fetch(fullURL);
             const data = await apiResponse.json();
             const isValid = data.is_valid_format.value;
-            // console.log("start here", email)
-            // console.log(data)
-            // console.log(data.email, "check email", isValid)
-            // return isValid;
-            // const isValid = true
-            // const deliverability = "UNDELIVERABLE"
+           
             if (data.deliverability === "DELIVERABLE") {
-                // console.log(data.deliverability, "check email", isValid)
                 return isValid;
             }
             else {
-                // console.log(data.deliverability, "wrong email", isValid)
                 return false;
             }
-
-            // return false;
         }
 
     },
@@ -119,9 +108,7 @@ export default {
 </script>
 
 <style scoped>
-/* .section .columns{
-    align-items: flex-start;
-} */
+
 .is-invalid {
     border: 2px solid red;
 }
--- src/components/customersFeedback.vue
+++ src/components/customersFeedback.vue
@@ -1,212 +1,156 @@
 <template>
-    <div class="customer">
-        <section class="section" id="customer">
-            <div class="columns">
-                <div class="column is-12">
-                    <div class="card sliderWrapper">
-                        <vue-testimonials :items="items" arrow-color="#000000" ball-active-color-class="text-red-500"
-                            ball-inactive-color-class="text-red-500" title-classes="text-red-500 text-2xl"
-                            content-classes="text-red-500 text-8xl" />
-                    </div>
-                </div>
-            </div>
-        </section>
-    </div>
+  <div class="customer">
+    <section class="section" id="customer">
+      <div class="columns">
+        <div class="column is-12">
+          <div class="card sliderWrapper">
+            <vue-testimonials
+              :items="items"
+              arrow-color="#000000"
+              ball-active-color-class="text-purple-500"
+              ball-inactive-color-class="text-purple-500"
+              title-classes="text-purple-500 text-2xl"
+              content-classes="text-purple-500 text-8xl"
+              author-classes="text-purple-500 text-8xl"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
-<!-- <template>
-<vue-testimonials :items="items" 
-arrow-color="#000000" 
-ball-active-color-class="text-red-500" 
-ball-inactive-color-class="text-red-500" 
-title-classes="text-red-500 text-2xl" 
-content-classes="text-red-500 text-8xl"/>
-</template> -->
 
-<script >
-import { defineComponent, ref } from "vue"
+<script>
+import { defineComponent, ref } from "vue";
 // import image in vite
-import imageUrl from '@/assets/profilePic/default.jpg'
+import imageUrl from "@/assets/profilePic/default.jpg";
 
 export default defineComponent({
-    name: "CustomersFeedback",
-    components: {
-    },
-    setup() {
-        const items = ref([
-            {
-                title: 'Timely Support',
-                content: 'The support I received was fast and my issue was addressed with a single interaction. Awesome!',
-                image: imageUrl,
-                author: 'John Smith',
+  name: "CustomersFeedback",
+  components: {},
+  setup() {
+    const items = ref([
+      {
+        title: "Timely Support",
+        content:
+          "The support I received was fast and my issue was addressed with a single interaction. Awesome!",
+        image: imageUrl,
+        author: "Total Wellness",
+      },
+      {
+        title: "Thank you",
+        content: "Thank you so much for your good work",
+        image: imageUrl,
+        author: "Pokot Tours",
+      },
+    ]);
+    return { items };
+  },
 
-            },
-            {
-                title: 'Thank you',
-                content: 'Thank you so much for your good work',
-                image: imageUrl,
-                author: 'Pokot Tours',
-
-            }
-        ])
-        return { items }
-    },
-    // methods: {
-        // getImageUrl(name) {
-            // return new URL(`./dir/${name}.png`, import.meta.url).href
-        // }
-    // },
-    // data() {
-    //     return {
-    //         items:[
-    //            {
-    //                 title: 'Excelent',
-    //                 content: 'good morning',
-    //                 // image: '../src/assets/profilePic/dp.png',
-    //                 author:'John Smith',
-
-    //             },
-    //             {
-    //                 title: 'Excelent',
-    //                 content: 'good morning',
-    //                 // image: '../src/assets/profilePic/dp.png',
-    //                 author:'John Smith',
-
-    //             }
-    //         ]
-    //     }
-    // },
-})
-
+});
 </script>
 
 <style scoped>
 .section {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
 }
-
-
-
-/*.section::before {
-    content: '';
-    position: absolute;
-    width: 15em;
-    height: 15em;
-    background: radial-gradient(#a989e5, #B8A0EA);
-    box-shadow: 0em 0em 8em 6em rgb(169, 138, 229);
-    border-radius: 50%;
-    border: 1px solid #a989e5;
-    border-radius: 50%;
-    transform: translate(-11rem, -2rem);
-}
-
-.section::after {
-    content: '';
-    position: absolute;
-    width: 18em;
-    height: 18em;
-    background: radial-gradient(#faa953, #FBBB75);
-    box-shadow: 0em 0em 9em 6em #faaa53;
-    border-radius: 50%;
-    border: 1px solid #faa953;
-    transform: translate(15rem, 5rem);
-}*/
 
 .column.is-12 {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
 }
 
 .card {
-    width: 80%;
-    min-height: 20em;
-    max-height: 31em;
-    background: rgba(255, 255, 255, 0.1);
-    box-shadow: 0 25px 45px rgba(0, 0, 0, 0.1);
-    border: 1px solid rgba(255, 255, 255, 0.5);
-    border-right: 1px solid rgba(255, 255, 255, 0.2);
-    border-bottom: 1px solid rgba(255, 255, 255, 0.2);
-    border-radius: 0.412em;
-    margin: 2rem;
-    padding: 1em;
-    display: flex;
-    align-items: center;
-    justify-content: space-evenly;
-    /* flex-direction: column; */
-    z-index: 40;
+  width: 80%;
+  min-height: 20em;
+  max-height: 31em;
+  background: rgba(255, 255, 255, 0.1);
+  box-shadow: 0 25px 45px rgba(0, 0, 0, 0.1);
+  border: 1px solid rgba(255, 255, 255, 0.5);
+  border-right: 1px solid rgba(255, 255, 255, 0.2);
+  border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+  border-radius: 0.412em;
+  margin: 2rem;
+  padding: 1em;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  /* flex-direction: column; */
+  z-index: 40;
 }
 
 .is-dark-mode-active .card {
-    background: rgba(255, 255, 255, 0.1);
-    box-shadow: 0 25px 45px rgba(0, 0, 0, 0.1);
-    border: 1px solid rgba(255, 255, 255, 0.5);
-    border-right: 1px solid rgba(255, 255, 255, 0.2);
-    border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+  background: rgba(255, 255, 255, 0.1);
+  box-shadow: 0 25px 45px rgba(0, 0, 0, 0.1);
+  border: 1px solid rgba(255, 255, 255, 0.5);
+  border-right: 1px solid rgba(255, 255, 255, 0.2);
+  border-bottom: 1px solid rgba(255, 255, 255, 0.2);
 }
 
 .card .cardHeader {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .card .cardHeader .profileImg {
-    width: 5em;
-    height: 5em;
+  width: 5em;
+  height: 5em;
 }
 
 .card .cardHeader .profileImg img {
-    width: 100%;
-    height: 100%;
-    border-radius: 50%;
-    object-fit: contain;
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  object-fit: contain;
 }
 
 .card .cardBody {
-    width: 90%;
+  width: 90%;
 }
 
 .card .cardBody .feedback {
-    width: 100%;
-    margin: 1em 0;
-    /* border: 2px solid red; */
+  width: 100%;
+  margin: 1em 0;
+  /* border: 2px solid red; */
 }
 </style>
 
 <style>
-.text-red-500 {
-    color: #a989e5 !important;
+.text-purple-500 {
+  color: #a989e5 !important;
 }
 
 .mb-40 {
-    margin-bottom: 0 !important;
+  margin-bottom: 0 !important;
 }
 
 .mt-6 {
-    margin-top: 0 !important;
+  margin-top: 0 !important;
 }
 
-.ball{
-    border-radius: 1px solid red;
+.ball {
+  border-radius: 1px solid red;
 }
 
-.text-red-500.text-8xl.mt-5.text-lg{
-    margin: 10px 50px;
-    font-weight: normal;
-    font-style: italic;
-    color: #faa953 !important;
+.text-purple-500.text-8xl.mt-5.text-lg {
+  margin: 10px 50px;
+  font-weight: normal;
+  font-style: italic;
+  color: #faa953 !important;
 }
 @media (min-width: 1024px) {
-    .lg\:pb-20 {
-        padding-bottom: 0 !important;
-    }
+  .lg\:pb-20 {
+    padding-bottom: 0 !important;
+  }
 
-    .ml-6 {
-        margin-top: 0 !important;
-    }
+  .ml-6 {
+    margin-top: 0 !important;
+  }
 }
 </style>
